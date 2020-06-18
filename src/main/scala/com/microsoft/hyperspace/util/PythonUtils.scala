c69c63e0dabd6fc90c59a1191c61f5131491da4f
Tarun <tarunr211@gmail.com>
2020-06-18 16:13:54 -0400

    Adding Wrapper util for C# or python bindings.

---
+++ src/main/scala/com/microsoft/hyperspace/util/PythonUtils.scala
@@ -0,0 +1,41 @@
+package com.microsoft.hyperspace.util
+
+import org.apache.spark.sql.SparkSession
+
+import com.microsoft.hyperspace.Implicits
+
+/**
+ * Provides helper methods for python APIs.
+ */
+object PythonUtils {
+
+  /**
+   * Wrapper for Implicits.enableHyperspace to be called from the Python API.
+   *
+   * @param spark SparkSession
+   * @return a spark session that contains Hyperspace-specific rules.
+   */
+  def enableHyperspace(spark: SparkSession): SparkSession = {
+    spark.enableHyperspace()
+  }
+
+  /**
+   * Wrapper for Implicits.disableHyperspace to be called from the Python API.
+   *
+   * @param spark SparkSession
+   * @return a spark session that does not contain Hyperspace-specific rules.
+   */
+  def disableHyperspace(spark: SparkSession): SparkSession = {
+    spark.disableHyperspace()
+  }
+
+  /**
+   * Wrapper for Implicits.isHyperspaceEnabled to be called from the Python API.
+   *
+   * @param spark SparkSession
+   * @return true if Hyperspace is enabled or false otherwise.
+   */
+  def isHyperspaceEnabled(spark: SparkSession): Boolean = {
+    spark.isHyperspaceEnabled()
+  }
+}
