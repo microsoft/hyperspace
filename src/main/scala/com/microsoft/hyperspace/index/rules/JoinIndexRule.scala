e261282bde0abcc7263adf0e9ccb3950d3009bd0
apoorvedave1 <66283785+apoorvedave1@users.noreply.github.com>
2020-06-19 16:54:35 -0700

    Cleanup scaladoc for join rule (#14)

--- src/main/scala/com/microsoft/hyperspace/index/rules/JoinIndexRule.scala
+++ src/main/scala/com/microsoft/hyperspace/index/rules/JoinIndexRule.scala
@@ -23,8 +23,8 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.analysis.CleanupAliases
 import org.apache.spark.sql.catalyst.catalog.BucketSpec
-import org.apache.spark.sql.catalyst.expressions.{And, Attribute, AttributeReference, EqualTo, Expression}
-import org.apache.spark.sql.catalyst.plans.logical.{Join, LogicalPlan}
+import org.apache.spark.sql.catalyst.expressions.{Alias, And, Attribute, AttributeReference, AttributeSet, EqualTo, Expression}
+import org.apache.spark.sql.catalyst.plans.logical.{Join, LogicalPlan, Project}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, InMemoryFileIndex, LogicalRelation}
 import org.apache.spark.sql.execution.datasources.parquet.ParquetFileFormat
