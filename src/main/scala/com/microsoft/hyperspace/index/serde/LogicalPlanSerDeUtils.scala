2ac339999762e6d7af44fe6ffadeb598c63ede0f
Apoorve Dave <apoorve.iitr@gmail.com>
2020-07-07 14:32:38 -0700

    add checks for JSON format and test fixes

--- src/main/scala/com/microsoft/hyperspace/index/serde/LogicalPlanSerDeUtils.scala
+++ src/main/scala/com/microsoft/hyperspace/index/serde/LogicalPlanSerDeUtils.scala
@@ -25,6 +25,7 @@ import org.apache.spark.sql.catalyst.expressions.{Exists, InSubquery, ListQuery,
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.execution.datasources._
 import org.apache.spark.sql.execution.datasources.csv.CSVFileFormat
+import org.apache.spark.sql.execution.datasources.json.JsonFileFormat
 import org.apache.spark.sql.execution.datasources.parquet.ParquetFileFormat
 
 import com.microsoft.hyperspace.HyperspaceException
@@ -222,16 +223,18 @@ object LogicalPlanSerDeUtils {
 
   object ExtractFileFormat {
     def unapply(fileFormat: FileFormat): Option[FileFormat] = fileFormat match {
+      case CSVFileFormatWrapper => Some(new CSVFileFormat)
+      case JsonFileFormatWrapper => Some(new JsonFileFormat)
       case f: ParquetFileFormat => Some(f)
-      case _: CSVFileFormat => Some(CSVFileFormatWrapper)
       case _ => throw HyperspaceException("Unsupported File Format found.")
     }
   }
 
   object ExtractSerializableFileFormat {
     def unapply(fileFormat: FileFormat): Option[FileFormat] = fileFormat match {
+      case _: CSVFileFormat => Some(CSVFileFormatWrapper)
+      case _: JsonFileFormat => Some(JsonFileFormatWrapper)
       case f: ParquetFileFormat => Some(f)
-      case CSVFileFormatWrapper => Some(new CSVFileFormat)
       case _ => throw HyperspaceException("Unsupported File Format found.")
     }
   }
