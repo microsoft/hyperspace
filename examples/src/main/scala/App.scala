b1d9e9963abb86b25e94a9d9bebb95a1b79d8955
Pouria Pirzadeh <pouriap@microsoft.com>
2020-07-14 15:34:50 -0700

    add Hyperspace HelloWorld app

---
+++ examples/src/main/scala/App.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright (2020) The Hyperspace Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import com.microsoft.hyperspace._
+import com.microsoft.hyperspace.Hyperspace
+import com.microsoft.hyperspace.index.{IndexConfig, IndexConstants}
+
+object App {
+  def main(args: Array[String]): Unit = {
+    // Create Spark session
+    val sparkConf = new SparkConf().setMaster("local[*]")
+    val spark = SparkSession.builder.config(sparkConf).getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    // Sample department records
+    val departments = Seq(
+      (10, "Accounting", "New York"),
+      (20, "Research", "Dallas"),
+      (30, "Sales", "Chicago"),
+      (40, "Operations", "Boston"))
+
+    // Sample employee records
+    val employees = Seq(
+      (7369, "SMITH", 20),
+      (7499, "ALLEN", 30),
+      (7521, "WARD", 30),
+      (7566, "JONES", 20),
+      (7698, "BLAKE", 30),
+      (7782, "CLARK", 10),
+      (7788, "SCOTT", 20),
+      (7839, "KING", 10),
+      (7844, "TURNER", 30),
+      (7876, "ADAMS", 20),
+      (7900, "JAMES", 30),
+      (7934, "MILLER", 10),
+      (7902, "FORD", 20),
+      (7654, "MARTIN", 30))
+
+    // Save example data records as Parquet
+    import spark.implicits._
+    val deptLocation = "departments"
+    val empLocation = "employees"
+    departments
+      .toDF("deptId", "deptName", "location")
+      .write
+      .mode("overwrite")
+      .parquet(deptLocation)
+
+    employees
+      .toDF("empId", "empName", "deptId")
+      .write
+      .mode("overwrite")
+      .parquet(empLocation)
+
+    // Create Hyperspace indexes
+    val hyperspace = new Hyperspace(spark)
+
+    val empDF: DataFrame = spark.read.parquet(empLocation)
+    val deptDF: DataFrame = spark.read.parquet(deptLocation)
+
+    val empIndexConfig: IndexConfig = IndexConfig("empIndex", Seq("deptId"), Seq("empName"))
+    val deptIndexConfig: IndexConfig = IndexConfig("deptIndex", Seq("deptId"), Seq("deptName"))
+    hyperspace.createIndex(empDF, empIndexConfig)
+    hyperspace.createIndex(deptDF, deptIndexConfig)
+
+    // List all indexes
+    hyperspace.indexes.show
+
+    // Enable Hyperspace to leverage indexes
+    spark.enableHyperspace()
+    val deptDFrame: DataFrame = spark.read.parquet("departments")
+    val empDFrame: DataFrame = spark.read.parquet(empLocation)
+
+    // Example of index usage for filtered selection
+    val eqFilter: DataFrame = deptDFrame.filter("deptId = 20").select("deptName")
+    eqFilter.show()
+    hyperspace.explain(eqFilter)
+
+    // Example of index usage for join
+    val eqJoin: DataFrame =
+      empDFrame.
+        join(deptDFrame, empDFrame("deptId") === deptDFrame("deptId")).
+        select(empDFrame("empName"), deptDFrame("deptName"))
+    eqJoin.show()
+    hyperspace.explain(eqJoin)
+  }
+}
