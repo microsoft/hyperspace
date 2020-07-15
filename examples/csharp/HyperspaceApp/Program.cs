7ef9fabc302d6c1eed842298806cb696a76d7e50
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2020-07-15 12:02:02 -0700

    Initial commit

---
+++ examples/csharp/HyperspaceApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HyperspaceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
