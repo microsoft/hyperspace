aad09cd6aa73727ef332dc6601cc0dadfeac0d68
Apoorve Dave <apoorve.iitr@gmail.com>
2020-10-12 16:06:53 -0700

    docs

--- src/main/scala/com/microsoft/hyperspace/actions/OptimizeAction.scala
+++ src/main/scala/com/microsoft/hyperspace/actions/OptimizeAction.scala
@@ -25,6 +25,36 @@ import com.microsoft.hyperspace.index.DataFrameWriterExtensions.Bucketizer
 import com.microsoft.hyperspace.telemetry.{AppInfo, HyperspaceEvent, OptimizeActionEvent}
 import com.microsoft.hyperspace.util.HyperspaceConf
 
+/**
+ * Optimize Action provides an optimize support for indexes where small index files
+ * can be merged into larger ones for better index performance.
+ *
+ * Algorithm outline:
+ * 1. Collect all the currently valid index files.
+ * 2. Split files into small and large, based on a threshold.
+ * 3. Bucketwise combine smaller files into 1 file per bucket.
+ * 4. Update index snapshot to remove small files and keep large files + newly created files.
+ *
+ * NOTE: This is an index-only operation. It does not look at the current state of
+ * the data at all. If the data was changed after index creation, optimize will NOT
+ * include the data changes.
+ *
+ * Available modes:
+ * `Quick` mode: This mode allows for fast optimization. Files smaller than a
+ * predefined threshold "spark.hyperspace.index.optimize.threshold" will be picked
+ * for compaction.
+ *
+ * `Full` mode: This allows for slow but complete optimization. ALL files are
+ * eligible for compaction. Compaction threshold is inifinity.
+ *
+ * TODO: Optimize can be a no-op if there is only one small file per bucket.
+ *  https://github.com/microsoft/hyperspace/issues/204.
+ *
+ * @param spark SparkSession
+ * @param logManager Index LogManager for index being refreshed
+ * @param dataManager Index DataManager for index being refreshed
+ * @param mode Acceptable optimize modes are `quick` and `full`.
+ */
 class OptimizeAction(
     spark: SparkSession,
     logManager: IndexLogManager,
