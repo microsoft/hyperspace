6ccf0f09d89552a71012d5aa060ce40d5f265e6b
EJ Song <sezruby@gmail.com>
2020-10-23 12:05:03 +0900

    minor fix

--- src/main/scala/com/microsoft/hyperspace/actions/OptimizeAction.scala
+++ src/main/scala/com/microsoft/hyperspace/actions/OptimizeAction.scala
@@ -16,8 +16,6 @@
 
 package com.microsoft.hyperspace.actions
 
-import scala.collection.mutable
-
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.SparkSession
