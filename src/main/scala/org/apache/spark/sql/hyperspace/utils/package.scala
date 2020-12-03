c82a3bc6577e9b168c8b1a38444debf143ed325c
Apoorve Dave <apoorve.iitr@gmail.com>
2020-12-02 18:22:37 -0800

    refactor

--- src/main/scala/org/apache/spark/sql/hyperspace/utils/package.scala
+++ src/main/scala/org/apache/spark/sql/hyperspace/utils/package.scala
@@ -16,8 +16,7 @@
 
 package org.apache.spark.sql.hyperspace
 
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 
 package object utils {
@@ -27,12 +26,7 @@ package object utils {
     }
   }
 
-  // This implementation is picked directly from:
-  // https://github.com/apache/spark/blob/v2.4.4/sql/core/src/main/scala/org/apache/spark/sql/
-  //  Dataset.scala#L76-L80
-  def logicalPlanToDataFrame(sparkSession: SparkSession, logicalPlan: LogicalPlan): DataFrame = {
-    val qe = sparkSession.sessionState.executePlan(logicalPlan)
-    qe.assertAnalyzed()
-    new Dataset[Row](sparkSession, qe, RowEncoder(qe.analyzed.schema))
+  def logicalPlanToDataFrame(spark: SparkSession, logicalPlan: LogicalPlan): DataFrame = {
+    Dataset.ofRows(spark, logicalPlan)
   }
 }
