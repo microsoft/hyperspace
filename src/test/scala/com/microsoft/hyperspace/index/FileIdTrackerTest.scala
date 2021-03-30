5147a3306a948743fc1659d3e15f7980b9594eb5
Chungmin Lee <lemonedo@gmail.com>
2021-03-30 11:10:13 +0900

    Get rid of demons

--- src/test/scala/com/microsoft/hyperspace/index/FileIdTrackerTest.scala
+++ src/test/scala/com/microsoft/hyperspace/index/FileIdTrackerTest.scala
@@ -34,7 +34,7 @@ class FileIdTrackerTest extends SparkFunSuite {
 
   test("getFileId returns None on unseen file properties.") {
     val tracker = new FileIdTracker
-    assert(tracker.getFileId(path = "abc", size = 123, modifiedTime = 666).isEmpty)
+    assert(tracker.getFileId(path = "abc", size = 123, modifiedTime = 555).isEmpty)
   }
 
   test("addFileInfo does nothing with an empty files set.") {
@@ -47,7 +47,7 @@ class FileIdTrackerTest extends SparkFunSuite {
   test("addFileInfo throws an exception if there is a FileInfo having an invalid file id.") {
     val tracker = new FileIdTracker
     val ex = intercept[HyperspaceException] {
-      tracker.addFileInfo(Set(FileInfo("abc", 123, 666, IndexConstants.UNKNOWN_FILE_ID)))
+      tracker.addFileInfo(Set(FileInfo("abc", 123, 555, IndexConstants.UNKNOWN_FILE_ID)))
     }
     assert(ex.getMessage.contains("Cannot add file info with unknown id"))
   }
@@ -56,7 +56,7 @@ class FileIdTrackerTest extends SparkFunSuite {
     "addFileInfo throws an exception if there is a conflict but modifications " +
       "before the exception are retained.") {
     val tracker = new FileIdTracker
-    tracker.addFileInfo(Set(FileInfo("def", 123, 666, 10)))
+    tracker.addFileInfo(Set(FileInfo("def", 123, 555, 10)))
     val ex = intercept[HyperspaceException] {
       implicit def ordering: Ordering[FileInfo] = new Ordering[FileInfo] {
         override def compare(x: FileInfo, y: FileInfo): Int = {
@@ -64,37 +64,37 @@ class FileIdTrackerTest extends SparkFunSuite {
         }
       }
       tracker.addFileInfo(scala.collection.immutable.SortedSet(
-        FileInfo("abc", 100, 666, 15),
-        FileInfo("def", 123, 666, 11)))
+        FileInfo("abc", 100, 555, 15),
+        FileInfo("def", 123, 555, 11)))
     }
     assert(ex.getMessage.contains("Adding file info with a conflicting id"))
-    assert(tracker.getFileId("abc", 100, 666).contains(15))
+    assert(tracker.getFileId("abc", 100, 555).contains(15))
   }
 
   test("addFileInfo puts new records in the map and increase the max id on success.") {
     val tracker = new FileIdTracker
-    tracker.addFileInfo(Set(FileInfo("abc", 123, 666, 10), FileInfo("def", 234, 777, 5)))
-    assert(tracker.getFileId("abc", 123, 666).contains(10))
+    tracker.addFileInfo(Set(FileInfo("abc", 123, 555, 10), FileInfo("def", 234, 777, 5)))
+    assert(tracker.getFileId("abc", 123, 555).contains(10))
     assert(tracker.getFileId("def", 234, 777).contains(5))
     assert(tracker.getMaxFileId == 10)
   }
 
   test("addFile returns the existing id and max id is unchanged.") {
     val tracker = new FileIdTracker
-    tracker.addFileInfo(Set(FileInfo("abc", 123, 666, 10)))
+    tracker.addFileInfo(Set(FileInfo("abc", 123, 555, 10)))
     assert(tracker.getMaxFileId == 10)
-    assert(tracker.addFile(new FileStatus(123, false, 3, 1, 666, new Path("abc"))) == 10)
+    assert(tracker.addFile(new FileStatus(123, false, 3, 1, 555, new Path("abc"))) == 10)
     assert(tracker.getMaxFileId == 10)
   }
 
   test("addFile returns a new id and max id is updated.") {
     val tracker = new FileIdTracker
-    assert(tracker.addFile(new FileStatus(123, false, 3, 1, 666, new Path("abc"))) == 0)
-    assert(tracker.addFile(new FileStatus(123, false, 3, 1, 666, new Path("def"))) == 1)
+    assert(tracker.addFile(new FileStatus(123, false, 3, 1, 555, new Path("abc"))) == 0)
+    assert(tracker.addFile(new FileStatus(123, false, 3, 1, 555, new Path("def"))) == 1)
     assert(tracker.addFile(new FileStatus(124, false, 3, 1, 777, new Path("xyz"))) == 2)
     assert(tracker.getMaxFileId == 2)
-    assert(tracker.getFileId("abc", 123, 666).contains(0))
-    assert(tracker.getFileId("def", 123, 666).contains(1))
+    assert(tracker.getFileId("abc", 123, 555).contains(0))
+    assert(tracker.getFileId("def", 123, 555).contains(1))
     assert(tracker.getFileId("xyz", 124, 777).contains(2))
   }
 }
