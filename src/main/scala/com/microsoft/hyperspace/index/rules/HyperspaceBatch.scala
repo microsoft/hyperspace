1f10e78d7191a837cdb1f23fbd5d3f019ab44e07
EJ Song <sezruby@gmail.com>
2021-04-21 14:03:41 -0700

    Add HyperspaceOneRule related definitions

---
+++ src/main/scala/com/microsoft/hyperspace/index/rules/HyperspaceBatch.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright (2020) The Hyperspace Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.hyperspace.index.rules
+
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+
+import com.microsoft.hyperspace.ActiveSparkSession
+import com.microsoft.hyperspace.index.IndexLogEntry
+
+trait HyperspaceBatch extends ActiveSparkSession {
+  val planChecks: Seq[HyperspacePlanCheck]
+
+  def applyIndex(plan: LogicalPlan, indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): LogicalPlan
+
+  def score(plan: LogicalPlan, indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): Int
+
+  final def apply(
+      plan: LogicalPlan,
+      indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): (LogicalPlan, Int) = {
+    if (indexes.isEmpty) {
+      return (plan, 0)
+    }
+
+    val candidateIndexes = planChecks.foldLeft(indexes) { (pti, check) =>
+      check(plan, pti)
+    }
+
+    if (candidateIndexes.nonEmpty) {
+      (applyIndex(plan, candidateIndexes), score(plan, candidateIndexes))
+    } else {
+      (plan, 0)
+    }
+  }
+}
+
+object NoOpBatch extends HyperspaceBatch {
+  override val planChecks = Nil
+
+  override def applyIndex(
+      plan: LogicalPlan,
+      indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): LogicalPlan = plan
+
+  override def score(plan: LogicalPlan, indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): Int = 0
+}
---
+++ src/main/scala/com/microsoft/hyperspace/index/rules/HyperspaceOneRule.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright (2020) The Hyperspace Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.microsoft.hyperspace.index.rules
+
+import scala.collection.mutable
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.catalyst.plans.logical.{LeafNode, LogicalPlan}
+import org.apache.spark.sql.catalyst.rules.Rule
+
+import com.microsoft.hyperspace.{ActiveSparkSession, Hyperspace}
+import com.microsoft.hyperspace.actions.Constants
+import com.microsoft.hyperspace.index.IndexLogEntry
+import com.microsoft.hyperspace.telemetry.HyperspaceEventLogging
+
+object CandidateIndexCollector extends ActiveSparkSession {
+  // TODO: ColumnSchemaCheck :: FileSignatureCheck :: Nil
+  val sourceChecks: Seq[HyperspaceSourceCheck] = Nil
+
+  def initializePlanToIndexes(
+      plan: LogicalPlan,
+      indexes: Seq[IndexLogEntry]): Map[LogicalPlan, Seq[IndexLogEntry]] = {
+    val provider = Hyperspace.getContext(spark).sourceProviderManager
+    plan.collect {
+      case l: LeafNode if provider.isSupportedRelation(l) =>
+        (l.asInstanceOf[LogicalPlan], indexes)
+    }.toMap
+  }
+
+  def apply(
+      plan: LogicalPlan,
+      allIndexes: Seq[IndexLogEntry]): Map[LogicalPlan, Seq[IndexLogEntry]] = {
+    val planToIndexes = initializePlanToIndexes(plan, allIndexes)
+    planToIndexes.flatMap { case (node, candidateIndexes) =>
+      Some(node, sourceChecks.foldLeft(candidateIndexes) { (indexes, check) =>
+        val res = check(node, indexes)
+        res
+      })
+        .filterNot(_._2.isEmpty)
+    }
+  }
+}
+
+class ScoreBasedIndexApplication {
+  val batches = NoOpBatch :: Nil // TODO: FilterIndexBatch :: JoinIndexBatch :: Nil
+
+  // Map for memoization
+  val scoreMap: mutable.HashMap[LogicalPlan, (LogicalPlan, Int)] = mutable.HashMap()
+
+  def recApply(
+      plan: LogicalPlan,
+      indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): (LogicalPlan, Int) = {
+    // If pre-calculated value exists, return it.
+    scoreMap.get(plan).foreach(res =>
+      return res
+    )
+
+    val optResult = (plan, 0)
+    // TODO apply indexes recursively.
+
+    scoreMap.put(plan, optResult)
+    optResult
+  }
+
+  def apply(plan: LogicalPlan, indexes: Map[LogicalPlan, Seq[IndexLogEntry]]): LogicalPlan = {
+    recApply(plan, indexes)._1
+  }
+}
+
+object HyperspaceOneRule
+    extends Rule[LogicalPlan]
+    with Logging
+    with HyperspaceEventLogging
+    with ActiveSparkSession {
+
+  override def apply(plan: LogicalPlan): LogicalPlan = {
+    val indexManager = Hyperspace
+      .getContext(spark)
+      .indexCollectionManager
+    val allIndexes = indexManager.getIndexes(Seq(Constants.States.ACTIVE))
+    if (allIndexes.isEmpty) {
+      plan
+    } else {
+      try {
+        val candidateIndexes = CandidateIndexCollector(plan, allIndexes)
+        val res = new ScoreBasedIndexApplication().apply(plan, candidateIndexes)
+        res
+      }
+      catch { case _: Exception =>
+        plan
+      }
+    }
+  }
+}
